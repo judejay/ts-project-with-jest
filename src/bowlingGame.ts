aa6e8c9813d50891280b8dba1dffe9cf833127ad
judejay <judejay26@gmail.com>
2023-11-06 18:07:26 +0000

    psudo code and data structure for bowling

--- src/bowlingGame.ts
+++ src/bowlingGame.ts
@@ -1,3 +1,14 @@
-export const bowlingGame = (rolls: string) => {
+export const bowlingGame = (rolls: number[][]) => {
 
+// the score card has an array of arrays usually 2
+
+// if the first number of frame is 10 it is strike
+
+// if both numbers of frame equals 10 it is a spare
+
+// if the last frame is a strike, then you get 2 more throws
+
+// if the last frame is a spare , then you get another throw.
+
+    
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,14 @@
 import { bowlingGame } from '../src/bowlingGame';
 
 describe("test bowlingGame function", () => {
-    it("should return correct score for all strikes(300)", () => {
-        expect(bowlingGame("X, X, X, X, X, X, X, X,,X, X, X, X")).toBe(300);
+
+     it("should return correct score for all strikes(300)", () => {
+        expect(bowlingGame([[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0]])).toBe(300);
     });   
-    it("should return correct score for all spares(90)", () => {
-        expect(bowlingGame("9, 9, 9, 9, 9, 9, 9, 9, 9, 9")).toBe(90);
+    it("should return correct score for all rolls of and a miss(90)", () => {
+        expect(bowlingGame([[9, 0],[9, 0],[9, 0],[9, 0],[9, 0],[9, 0], [9, 0],[9, 0],[9, 0],[9, 0]])).toBe(190);
     }); 
-    it("should return correct score for 21 rolls of 5", () => {
-        expect(bowlingGame("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 ")).toBe(150);
+    it("should return correct score all spares with a final 5(150)", () => {
+        expect(bowlingGame([[5, 5], [5, 5], [5, 5], [5, 5], [5, 5], [5, 5], [5, 5],[ 5, 5], [5, 5], [5, 5],[5] ])).toBe(150);
     }); 
     });
