3d79ae3a1dea8b44ee70cb2fca357371621c3a0f
judejay <judejay26@gmail.com>
2023-11-08 16:12:54 +0000

    Rover moving off plateau Westwards

--- src/move/advance.ts
+++ src/move/advance.ts
@@ -1,5 +1,18 @@
 import { Grid } from "../types/types";
 
 export const advance = (position: Grid): Grid => {
-    return [0, 1, "N"]
+    let [x, y, o] = position;
+    switch (o) {
+        case "N":
+            return [x, y + 1, o ]            
+            break;
+            case "E":
+                return [x + 1, y, o ]            
+                break;
+            case "W":
+                return[((x + 5 - 1) % 5), y, o]    
+        default:
+            break;
+    }
+    return [x, y, o]
 }
--- tests/move.test.ts
+++ tests/move.test.ts
@@ -30,7 +30,13 @@ describe("Rover should move according to command", () => {
     it("Rover should spin right from East", () => {
         expect(move("R", [0, 0, "E"])).toStrictEqual( [0, 0, "S"]);
     });
-    it("Rover should advance one unit", () => {
+    it("Rover should advance one unit North from initial position", () => {
         expect(advance( [0, 0, "N"])).toStrictEqual([0, 1, "N"]);
     }); 
+    it("Rover should advance one unit East from initial position", () => {
+        expect(advance( [0, 0, "E"])).toStrictEqual([1, 0, "E"]);
+    });
+    it("Rover should advance one unit West from initial position", () => {
+        expect(advance( [0, 0, "W"])).toStrictEqual([4, 0, "W"]);
+    });
     });
