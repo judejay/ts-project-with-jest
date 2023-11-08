fb1ab584f8b211306e3dc56ba0e12b8aad434913
judejay <judejay26@gmail.com>
2023-11-08 13:59:17 +0000

    Rover spins right

--- src/move/spin.ts
+++ src/move/spin.ts
@@ -1,3 +1,17 @@
-export const spin = (direction: string) => {
+import { Grid } from "../types/types";
 
+export const spin = (direction: string, position: Grid) => {
+    let [x, y, o ] = position
+switch (direction) {
+    case "R":
+        if(o === "N") {
+           o = "E"
+            return [x, y, o]
+        }
+        
+        break;
+
+    default:
+        break;
+}
 }
--- tests/move.test.ts
+++ tests/move.test.ts
@@ -7,10 +7,10 @@ describe("rover should move according to command", () => {
         expect(resetPosition()).toStrictEqual([0, 0, "N"])
     });
     it("should spin left", () => {
-        expect(spin("L")).toBe("spin");
+        expect(spin("L", [0, 0, "N"])).toBe("W");
     });   
     it("rover should spin right", () => {
-        expect(spin("R")).toBe("spin");
+        expect(spin("R", [0, 0, "N"])).toStrictEqual( [0, 0, "E"]);
     }); 
     it("rover should advance one unit", () => {
         expect(advance("M")).toBe("moved");
