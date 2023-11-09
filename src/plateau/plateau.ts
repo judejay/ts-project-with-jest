cf5519ea5f96ac71791156102c03259cb0452a17
judejay <judejay26@gmail.com>
2023-11-09 11:57:30 +0000

    format document

--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,27 +1,26 @@
-import * as readline from 'readline';
+import * as readline from "readline";
 
 export function Ask(query: any) {
-    const readline = require("readline").createInterface({
+  const readline = require("readline").createInterface({
     input: process.stdin,
-    output: process.stdout
-});
-return  new Promise<number[]>(resolve => readline.question(query, (ans: string)  => {
-    readline.close();
-    resolve(inputToNumber(ans));
-    
-  }))
+    output: process.stdout,
+  });
+  return new Promise<number[]>((resolve) =>
+    readline.question(query, (ans: string) => {
+      readline.close();
+      resolve(inputToNumber(ans));
+    })
+  );
 }
 
 const inputToNumber = (input: string): number[] => {
-      const arr=  input.split(" ")
-      var numbers = arr.map(function(str) {
-        return parseInt(str); });
-      console.log(numbers)
-    return numbers;
-}
+  const arr = input.split(" ");
+  var numbers = arr.map(function (str) {
+    return parseInt(str);
+  });
+  return numbers;
+};
 
 export async function plateau() {
-
-    var name = await Ask("Input grid X & Y \n")
+  var name = await Ask("Input grid X & Y \n");
 }
-
