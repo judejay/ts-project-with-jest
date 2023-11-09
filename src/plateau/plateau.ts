5913ccd82301b00376228e041d55c1ec6eed1d89
judejay <judejay26@gmail.com>
2023-11-09 11:51:55 +0000

    async test for input

---
+++ src/plateau/plateau.ts
@@ -0,0 +1,27 @@
+import * as readline from 'readline';
+
+export function Ask(query: any) {
+    const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+return  new Promise<number[]>(resolve => readline.question(query, (ans: string)  => {
+    readline.close();
+    resolve(inputToNumber(ans));
+    
+  }))
+}
+
+const inputToNumber = (input: string): number[] => {
+      const arr=  input.split(" ")
+      var numbers = arr.map(function(str) {
+        return parseInt(str); });
+      console.log(numbers)
+    return numbers;
+}
+
+export async function plateau() {
+
+    var name = await Ask("Input grid X & Y \n")
+}
+
